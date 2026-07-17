86978c932be51ba0a5a616f1cffe76befc4644b6
Patricia Souza <patricia.sz.br@gmail.com>
2026-07-17 11:52:20 -0300

    conversao

---
+++ 01_variaveis/02_conversao.py
@@ -0,0 +1,17 @@
+name = "Jonh"
+age = 30
+age2 = 20
+
+temp = name + " tem " + str(age)
+temp = bool(age) + age2
+print(temp)
+
+
+number = int("43")
+
+name = "Lenina"
+age = 30
+
+student = True
+
+print(f"{name } tem {age} anos é estudante: {student} ")
