6ea110e934c8d3e8b729097295bb6caaa19c2b7a
Patricia Souza <patricia.sz.br@gmail.com>
2026-07-20 11:03:49 -0300

    operadores comparacao

---
+++ 02_operadores/03_operadores_comparacao.py
@@ -0,0 +1,17 @@
+a = 10
+b = 5
+
+print(f"A {a} é igual a B? {b}")
+print(a==b)
+
+print(f"A {a} é diferente de B? {b}")
+print(a!=b)
+
+print(f"A {a} é maior ou igual a B? {b}")
+print(a >=b)
+
+
+print(f"A {a} é menor ou igual a B? {b}")
+print(a <=b)
+
+
