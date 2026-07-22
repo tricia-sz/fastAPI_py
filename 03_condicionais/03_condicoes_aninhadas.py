43c136495b676ec86c22fd8dec30a1cbaa9c90b2
Patricia Souza <patricia.sz.br@gmail.com>
2026-07-22 15:11:12 -0300

    if aninhado

---
+++ 03_condicionais/03_condicoes_aninhadas.py
@@ -0,0 +1,11 @@
+idade = 20
+tem_carteira = True
+
+if idade >= 18:
+  print("Voce é maior de idade")
+  if tem_carteira:
+    print("Voce pode dirigir")
+  else: 
+    print("Voce nao tem carteira, nao pode dirigir")
+else:
+  print("Voce é maior de idade")
